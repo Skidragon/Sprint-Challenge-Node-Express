1ad9ce6835746cfb147ec62ca75af19a8c792e76
Simon <Simon_Freelance@hotmail.com>
2018-07-27 11:45:02 -0400

    GET request completed for projects

--- api/projectRouter.js
+++ api/projectRouter.js
@@ -5,5 +5,17 @@ const projectModel = require('./../data/helpers/projectModel');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    projectModel.get()
+    .then(response => {
+        res.status(codes.OK).json(response);
+    })
+    .catch(() => {
+        next({
+            code: codes.INTERNAL_SERVER_ERROR,
+            message: 'Problem getting projects, try again later'
+        })
+    });
+});
 
 module.exports = router;
