92237de9b2cecbe16c2d850da9ed6cdb414620e4
Simon <Simon_Freelance@hotmail.com>
2018-07-27 13:50:40 -0400

    Finished getting action by id

--- api/actionRouter.js
+++ api/actionRouter.js
@@ -1,48 +1,68 @@
-const codes = require('./../data/helpers/statusCodes');
+const codes = require("./../data/helpers/statusCodes");
 
-const express = require('express');
-const actionModel = require('./../data/helpers/actionModel');
+const express = require("express");
+const actionModel = require("./../data/helpers/actionModel");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    actionModel.get()
+router.get("/", (req, res, next) => {
+  actionModel
+    .get()
     .then(response => {
-        res.status(codes.OK).json(response);
+      res.status(codes.OK).json(response);
     })
     .catch(err => {
-        next(err);
+      next(err);
+    });
+});
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  actionModel
+    .get(id)
+    .catch(() => {
+        throw {
+            code: codes.NOT_FOUND,
+            message: "The action with that id does not exist"
+          };
     })
-})
+    .then(response => {
+      res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+router.post("/", (req, res, next) => {
+  actionModel
+    .insert(req.body)
+    .then(response => {
+      res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+router.put("/:id", (req, res, next) => {
+  const { id } = req.params;
 
-router.post('/', (req,res, next) => {
-    actionModel.insert(req.body)
+  actionModel
+    .update(id, req.body)
     .then(response => {
-        res.status(codes.OK).json(response)
+      res.status(codes.OK).json(response);
     })
     .catch(err => {
-        next(err);
-    })
-})
-router.put('/:id', (req,res, next) => {
-    const { id } = req.params;
-
-    actionModel.update(id, req.body)
+      next(err);
+    });
+});
+router.delete("/:id", (req, res, next) => {
+  const { id } = req.params;
+  actionModel
+    .remove(id)
     .then(response => {
-        res.status(codes.OK).json(response);
+      res.status(codes.OK).json(response);
     })
     .catch(err => {
-        next(err);
-    })
-})
-router.delete('/:id', (req,res, next) => {
-    const { id } = req.params;
-    actionModel.remove(id)
-    .then(response => {
-        res.status(codes.OK).json(response);
-    })
-    .catch(err => {
-        next(err);
-    })
-})
+      next(err);
+    });
+});
 module.exports = router;
