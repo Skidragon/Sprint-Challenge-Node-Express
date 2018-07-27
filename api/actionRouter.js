ed54d242bc6c6940981e813a3e8a514dc3d97031
Simon <Simon_Freelance@hotmail.com>
2018-07-27 13:30:33 -0400

    GET request for all actions completed

--- api/actionRouter.js
+++ api/actionRouter.js
@@ -5,5 +5,13 @@ const actionModel = require('./../data/helpers/actionModel');
 
 const router = express.Router();
 
-
+router.get('/', (req, res, next) => {
+    actionModel.get()
+    .then(response => {
+        res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+        next(err);
+    })
+})
 module.exports = router;
